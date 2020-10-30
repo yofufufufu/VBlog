e3c50c0e700fc4955910c064a92f7d37609e4eab
yofufufufu <972633918@qq.com>
2020-10-30 18:25:18 +0800

    Update WebSecurityConfig.java

--- blogserver/src/main/java/org/sang/config/WebSecurityConfig.java
+++ blogserver/src/main/java/org/sang/config/WebSecurityConfig.java
@@ -39,8 +39,10 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
                 .antMatchers("/admin/category/all").authenticated()
-                .antMatchers("/admin/**","/reg").hasRole("超级管理员")///admin/**的URL都需要有超级管理员角色，如果使用.hasAuthority()方法来配置，需要在参数中加上ROLE_,如下.hasAuthority("ROLE_超级管理员")
-                .anyRequest().authenticated()//其他的路径都是登录后即可访问
+                // /admin/**的URL都需要有超级管理员角色，如果使用.hasAuthority()方法来配置，需要在参数中加上ROLE_,如下.hasAuthority("ROLE_超级管理员")
+                .antMatchers("/admin/**","/reg").hasRole("超级管理员")
+                // 其他的路径都是登录后即可访问
+                .anyRequest().authenticated()
                 .and().formLogin().loginPage("/login_page").successHandler(new AuthenticationSuccessHandler() {
             @Override
             public void onAuthenticationSuccess(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Authentication authentication) throws IOException, ServletException {
